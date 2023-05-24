c9682778e60df2cab65cdedc110038dac18c85a2
PoliSarieva <109367470+PoliSarieva@users.noreply.github.com>
2023-05-24 18:24:23 +0300

    Add cubeController

--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
 const express = require('express');
 const app = express();
-const cubeManager = require('./managers/cubeManager');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
 const homeController = require('./controllers/homeController');
+const createController = require('./controllers/cubeController');
 
 expressConfig(app);
 handlebarsConfig(app);
 
 app.use(homeController);
-
-app.get('/create', (req, res) => {
-    console.log(cubeManager.getAll());;
-    res.render('create');
-});
-
-app.post('/create', (req,res) => {
-    const { name, description, imageUrl, difficultyLevel } = req.body;
-
-    cubeManager.createCube({name, description, imageUrl, difficultyLevel});
-    res.redirect('/');
-});
-
+app.use('/cubes', createController);
 
 app.listen(5000, () => console.log('Server is running on port 5000'));
