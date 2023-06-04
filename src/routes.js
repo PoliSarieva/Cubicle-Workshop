1990e3838867169fa38271e2b215aba5b559cb1d
PoliSarieva <109367470+PoliSarieva@users.noreply.github.com>
2023-06-04 10:59:21 +0300

    Add create accessory page

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const createController = require('./controllers/cubeController');
+const accessoryController = require('./controllers/acccessoryController');
 
 router.use(homeController);
 router.use('/cubes', createController);
+router.use('/accessories', accessoryController);
 router.get('*', (res, req) => {
     req.render('404');
 });
