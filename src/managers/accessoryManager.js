534ab452be5793842f6161e1a042f49123ec8ef3
PoliSarieva <109367470+PoliSarieva@users.noreply.github.com>
2023-06-04 15:53:56 +0300

    Add accessory manager and add to db

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -1,2 +1,3 @@
+const Accessory = require('../models/Accessory');
 
-exports.create = (accessoryData) => console.log(accessoryData);
+exports.create = (accessoryData) => Accessory.create(accessoryData);
