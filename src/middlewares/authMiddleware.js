4cae061a03c49dbf00339fa2cace89b6abcfa5bb
PoliSarieva <109367470+PoliSarieva@users.noreply.github.com>
2023-06-09 00:55:23 +0300

    Add authentication

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,24 +1,32 @@
 const jwt = require('../lip/jwt');
 const { SECRET } = require('../config/config');
 
-exports.auth =async (req, res, next) => {
+exports.auth = async (req, res, next) => {
     const token = req.cookies['auth'];
-    
+
     if (token) {
         try {
             const decodedToken = await jwt.verify(token, SECRET);
             req.user = decodedToken;
             res.locals.user = decodedToken;
             res.locals.isAuthenticated = true;
-            
+
             next();
         } catch (err) {
             res.clearCookie('auth');
 
             return res.redirect('/users/login');
         }
-        
+
     } else {
-    next();
+        next();
     }
-}
+};
+
+exports.isAuth = (req, res, next) => {
+    if (!req.user) {
+        return res.redirect('user/login');
+    }
+
+    next();
+};
